2104105abfc77919ba7d1629bc63437940ca80f4
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-06 13:56:03 -0500

    Added MusicAlbumCollectionView and integrated with custom layout

---
+++ MusicAlbum/MusicAlbum/View/MusicAlbumCollectionView.swift
@@ -0,0 +1,36 @@
+//
+//  MusicAlbumCollectionView.swift
+//  MusicAlbum
+//
+//  Created by Pedro Trujillo on 1/6/21.
+//
+
+import UIKit
+
+class MusicAlbumCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    init(frame: CGRect) {
+        let layout = MusicAlbumUICollectionViewFlowLayout(frame: frame)
+        super.init(frame: frame, collectionViewLayout: layout)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        self.alwaysBounceVertical = true
+        self.backgroundColor = UIColor.red
+        // Register cell classes
+        self.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
+}
