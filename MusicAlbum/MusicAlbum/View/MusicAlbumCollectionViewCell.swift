a5cbd57efa43608dd1e1104b13325a09d1ae96fe
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-06 14:11:14 -0500

    Added MusicAlbumCollectionViewCell file

---
+++ MusicAlbum/MusicAlbum/View/MusicAlbumCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MusicAlbumCollectionViewCell.swift
+//  MusicAlbum
+//
+//  Created by Pedro Trujillo on 1/6/21.
+//
+
+import UIKit
+
+class MusicAlbumCollectionViewCell: UICollectionViewCell {
+    
+}
