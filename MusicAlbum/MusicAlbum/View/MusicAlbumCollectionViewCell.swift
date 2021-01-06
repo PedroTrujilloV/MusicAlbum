b4c94062a930270bc3814f3f9e42e0170f1c92c7
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-06 18:48:06 -0500

    Refactored MusicAlbumCollectionViewCell

--- MusicAlbum/MusicAlbum/View/MusicAlbumCollectionViewCell.swift
+++ MusicAlbum/MusicAlbum/View/MusicAlbumCollectionViewCell.swift
@@ -8,21 +8,70 @@
 import UIKit
 import Combine
 
+enum LoadingState {
+    case notLoading
+    case loading
+    case loaded(UIImage)
+}
+
 class MusicAlbumCollectionViewCell: UICollectionViewCell {
     static let reuserIdentifier: String = "MusicAlbumCollectionViewCellReuserIdentifier"
     private var cancellables: Array<AnyCancellable> = []
     private static let processingQueue = DispatchQueue(label: "processingQueue")
     private let heightProportion:CGFloat = 0.65
-    
     private let defaultImage = UIImage(named: "a-nikelogo")
     private let likeStateImageView = UIImageView(image: UIImage(systemName: "heart.fill") )
-    private var imageView = UIImageView()
-    private var albumNameLabel = UILabel()
-    private var artistNameLabel = UILabel()
     private let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
-    private let stackView   = UIStackView()
-    private let textStackView   = UIStackView()
     
+    private let stackView:UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis  = NSLayoutConstraint.Axis.vertical
+        stackView.distribution  = UIStackView.Distribution.fillProportionally
+        stackView.alignment = UIStackView.Alignment.center
+        stackView.spacing  = 8.0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let textStackView:UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis  = NSLayoutConstraint.Axis.vertical
+        stackView.distribution  = UIStackView.Distribution.fillEqually
+        stackView.alignment = UIStackView.Alignment.center
+        stackView.spacing   = 3.0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private var thumbnailImageView:UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.cellImageBackgroundColor
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 5
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "a-nikelogo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private var albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.text  = "No Album"
+        label.textAlignment = .left
+        label.font = UIFont(name: "AvenirNext-DemiBold", size: 12)
+        label.textColor = UIColor.albumNameTextColor
+        return label
+    }()
+    
+    private var artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.text  = "No Artist"
+        label.textAlignment = .left
+        label.font = UIFont(name: "AvenirNext-Regular", size: 11)
+        label.textColor = UIColor.artistNameTextColor
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -42,62 +91,38 @@ class MusicAlbumCollectionViewCell: UICollectionViewCell {
     }
     
     func setup(){
-        setupImageView()
-        setupText()
         setupStackView()
         setupIcons()
     }
-    
-    private func setupImageView(){
-        imageView.backgroundColor = UIColor.cellImageBackgroundColor
-        imageView.heightAnchor.constraint(equalToConstant: self.frame.width * heightProportion).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
-        imageView.contentMode = .scaleAspectFit
-        imageView.layer.cornerRadius = 5
-        imageView.layer.masksToBounds = true
-        imageView.image = defaultImage
-    }
-    
-    private func setupText(){
-        albumNameLabel.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
-        albumNameLabel.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
-        albumNameLabel.text  = "-$0.00 cash back :("
-        albumNameLabel.textAlignment = .left
-        albumNameLabel.font = UIFont(name: "AvenirNext-DemiBold", size: 12)
-        albumNameLabel.textColor = UIColor.albumNameTextColor
-        
-        artistNameLabel.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
-        artistNameLabel.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
-        artistNameLabel.text  = "none product :("
-        artistNameLabel.textAlignment = .left
-        artistNameLabel.font = UIFont(name: "AvenirNext-Regular", size: 11)
-        artistNameLabel.textColor = UIColor.artistNameTextColor
-    }
-    
+       
     private func setupStackView(){
-        textStackView.axis  = NSLayoutConstraint.Axis.vertical
-        textStackView.distribution  = UIStackView.Distribution.fillEqually
-        textStackView.alignment = UIStackView.Alignment.center
-        textStackView.spacing   = 3.0
+        setupImageViewConstraints()
+        setupTextConstraint()
 
-        textStackView.addArrangedSubview(albumNameLabel)
-        textStackView.addArrangedSubview(artistNameLabel)
-        textStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        stackView.axis  = NSLayoutConstraint.Axis.vertical
-        stackView.distribution  = UIStackView.Distribution.fillProportionally
-        stackView.alignment = UIStackView.Alignment.center
-        stackView.spacing   = 8.0
-
-        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(thumbnailImageView)
         stackView.addArrangedSubview(textStackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
 
         self.addSubview(stackView)
         activityIndicator.startAnimating()
         
         stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    }
+    
+    private func setupImageViewConstraints(){
+        thumbnailImageView.heightAnchor.constraint(equalToConstant: self.frame.width).isActive = true
+        thumbnailImageView.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
+    }
+    
+    private func setupTextConstraint(){
+        albumNameLabel.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
+        albumNameLabel.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+        
+        artistNameLabel.widthAnchor.constraint(equalToConstant: self.frame.width).isActive = true
+        artistNameLabel.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+
+        textStackView.addArrangedSubview(albumNameLabel)
+        textStackView.addArrangedSubview(artistNameLabel)
     }
     
     private func setupIcons(){
@@ -118,7 +143,7 @@ class MusicAlbumCollectionViewCell: UICollectionViewCell {
     
     
     override func prepareForReuse() {
-        imageView.image = defaultImage
+        thumbnailImageView.image = defaultImage
         self.likeStateImageView.isHidden = true
         self.cancel()
     }
