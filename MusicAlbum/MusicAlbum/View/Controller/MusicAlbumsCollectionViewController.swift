7d48bc55f8d9829e1e73e94a566f4ec1fc94e0e8
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-06 13:57:39 -0500

    Integrated Custom Collection view with view controller and custom layout

--- MusicAlbum/MusicAlbum/View/Controller/MusicAlbumsCollectionViewController.swift
+++ MusicAlbum/MusicAlbum/View/Controller/MusicAlbumsCollectionViewController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+let reuseIdentifier = "Cell"
 
 class MusicAlbumsCollectionViewController: UICollectionViewController {
+    
+    let numberOfItemsInSection = 23
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,45 +20,39 @@ class MusicAlbumsCollectionViewController: UICollectionViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        self.collectionView.backgroundColor = .orange
+        setup()
+    }
+    
+    private func setup(){
+        navigationItem.title = "Music Albums"
+        collectionView = MusicAlbumCollectionView(frame: collectionView.frame)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView.collectionViewLayout.invalidateLayout()
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+extension MusicAlbumsCollectionViewController { // MARK: UICollectionViewDataSource
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return numberOfItemsInSection
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        cell.backgroundColor = .purple
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
+    
+}
+    
+extension MusicAlbumsCollectionViewController { // MARK: UICollectionViewDelegate
+    
     /*
     // Uncomment this method to specify if the specified item should be highlighted during tracking
     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
