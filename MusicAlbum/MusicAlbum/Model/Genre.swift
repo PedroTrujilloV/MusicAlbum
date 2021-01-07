839e433fe0df07de1224a8a9f84c40f6c1a77c1b
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-07 00:26:15 -0500

    Added Genre Model

---
+++ MusicAlbum/MusicAlbum/Model/Genre.swift
@@ -0,0 +1,16 @@
+//
+//  Genre.swift
+//  MusicAlbum
+//
+//  Created by Pedro Trujillo on 1/7/21.
+//
+
+import Foundation
+
+struct Genre: Codable {
+
+    var genreId:String?
+    var name:String
+    var url:String?
+    
+}
