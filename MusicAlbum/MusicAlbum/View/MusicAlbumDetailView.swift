91e7e8a00ed6ea4d97c5952f080523a263fa819c
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-07 00:15:09 -0500

    Added MusicAlbumDetailView

---
+++ MusicAlbum/MusicAlbum/View/MusicAlbumDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MusicAlbumDetailView.swift
+//  MusicAlbum
+//
+//  Created by Pedro Trujillo on 1/7/21.
+//
+
+import UIKit
+
+class MusicAlbumDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
