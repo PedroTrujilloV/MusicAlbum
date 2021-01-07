4ed84f992428e1873beb7cbb37f0449babb10367
Pedro Trujillo V <ptrujillo@crunchy.co>
2021-01-07 01:13:26 -0500

    Added logic and integrated with Detail View

--- MusicAlbum/MusicAlbum/View/Controller/MusicAlbumDetailViewController.swift
+++ MusicAlbum/MusicAlbum/View/Controller/MusicAlbumDetailViewController.swift
@@ -6,24 +6,79 @@
 //
 
 import UIKit
+import Combine
 
-class MusicAlbumDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class MusicAlbumDetailViewController: UIViewController{
+    
+    private var detailView: MusicAlbumDetailView?
+    private var viewModel:MusicAlbumViewModel
+    private var cancellable:AnyCancellable?
+    private static let processingQueue = DispatchQueue(label: "processingQueue")
+    
+//    private let heartFillImage = UIImage(systemName: "heart.fill")
+    private let exitImage = UIImage(systemName: "xmark")
+    
+    
+    init(_ viewModel: MusicAlbumViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    deinit  {
+        self.cancel()
+    }
+    
+    private func cancel(){
+        cancellable?.cancel()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    func setup(){
+        bind()
+        setupView()
+        setupButtons()
+    }
+    
+    func bind(){
+        
+    }
+    
+    func setupButtons() {
+        let exitButton = UIBarButtonItem(image: exitImage,
+                                         style: UIBarButtonItem.Style.plain,
+                                         target: self,
+                                         action: #selector(closeAction(sender:)))
+        exitButton.tintColor = UIColor.systemGray
+        self.navigationItem.leftBarButtonItem = exitButton
+    }
+    
+    private func setupView() {
+        viewRespectsSystemMinimumLayoutMargins = false
+        self.title = "Details"
+        let view = UIView()
+        detailView = MusicAlbumDetailView(frame: self.view.frame, offerViewModel: self.viewModel)
+        view.addSubview(self.detailView ?? UIView())
+        self.view = view
+    }
+    
+    @objc private func likeItAction(sender:UIBarButtonItem) {
+        // do something
+    }
+    
+    
+    
+    @objc private func closeAction(sender:UIBarButtonItem) {
+        dismiss(animated: true) {
+            // do something
+        }
+    }
+       
 }
